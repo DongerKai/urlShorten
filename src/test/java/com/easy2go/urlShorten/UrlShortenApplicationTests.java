6e12b72f59af47bfbd6dc2bd40c9d61d15418a84
DongerKai <koekchan@gmail.com>
2022-05-14 14:05:26 +0800

    initial

---
+++ src/test/java/com/easy2go/urlShorten/UrlShortenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy2go.urlShorten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
